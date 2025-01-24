b6c23be62f3d9f37199ba06db2c4af12bc7fb37f
Alejandro Ollivier <aleollivierochoa@gmail.com>
2025-01-24 01:57:42 +0100

    Initial Commit

---
+++ TestWeather/TestWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestWeatherApp.swift
+//  TestWeather
+//
+//  Created by Alejandro Ollivier Ochoa on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TestWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
